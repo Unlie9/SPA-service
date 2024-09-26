e7ded4274ff1d45711467236055d2b14dfedc21c
unlie9 <unlie.devops9@gmail.com>
2024-09-26 07:32:18 +0300

    prepared directory Backend

--- Backend/comments_service/asgi.py
+++ Backend/comments_service/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for spa_service project.
+ASGI config for comments project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spa_service.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comments.settings')
 
 application = get_asgi_application()
--- Backend/comments_service/wsgi.py
+++ Backend/comments_service/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for spa_service project.
+WSGI config for comments project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spa_service.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comments.settings')
 
 application = get_wsgi_application()
