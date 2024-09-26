103ca3560621d23ae11645eb8eb2d7375acaca19
unlie9 <unlie.devops9@gmail.com>
2024-09-26 12:21:17 +0300

    added start basic models

---
+++ Backend/comments/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register("", "")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path('captcha/', include('captcha.urls')),
+]
