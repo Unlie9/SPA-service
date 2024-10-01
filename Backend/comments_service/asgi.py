b9a85587715d6b3791d1ce1ab42360ce5e017f23
unlie9 <unlie.devops9@gmail.com>
2024-10-01 11:47:01 +0300

    completed basic logic with websockets

--- Backend/comments_service/asgi.py
+++ Backend/comments_service/asgi.py
@@ -2,7 +2,8 @@ import os
 import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+from jwt_middleware import JwtAuthMiddlewareStack
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comments_service.settings')
 django.setup()
@@ -11,7 +12,7 @@ from comments import routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "websocket": JwtAuthMiddlewareStack(
         URLRouter(
             routing.websocket_urlpatterns
         )
