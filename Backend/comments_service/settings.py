80717e3b4cd17bbfe72d6dadf0485f4dd3b8cca0
unlie9 <unlie.devops9@gmail.com>
2024-10-07 11:44:33 +0300

    minor fixes

--- Backend/comments_service/settings.py
+++ Backend/comments_service/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '192.168.1.100']
 
