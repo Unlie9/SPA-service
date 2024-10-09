4ad5b7d6ff0b34b9ff4cae8b86a253e7548a44a5
unlie9 <unlie.devops9@gmail.com>
2024-10-09 22:53:00 +0300

    change serializers.py

--- Backend/comments/serializers.py
+++ Backend/comments/serializers.py
@@ -3,23 +3,6 @@ from rest_framework import serializers
 from comments.models import Comment
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    replies = serializers.SerializerMethodField()
-    reply = serializers.PrimaryKeyRelatedField(queryset=Comment.objects.all(), required=False, allow_null=True)
-    home_page = serializers.CharField(required=False, allow_blank=True, allow_null=True)
-
-    class Meta:
-        model = Comment
-        fields = ("id", "replies", "user", "text", "created_at", "reply", "home_page")
-        read_only_fields = ("user",)
-
-    @staticmethod
-    def get_replies(obj):
-        if isinstance(obj, Comment) and obj.replies.exists():
-            return CommentSerializer(obj.replies.all(), many=True).data
-        return None
-
-
 class CommentListSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source="user.username", read_only=True)
     replies = serializers.SerializerMethodField()
@@ -31,6 +14,6 @@ class CommentListSerializer(serializers.ModelSerializer):
 
     @staticmethod
     def get_replies(obj):
-        if isinstance(obj, Comment) and obj.replies.exists():
-            return CommentSerializer(obj.replies.all(), many=True).data
-        return None
+        if obj.replies.exists():
+            return CommentListSerializer(obj.replies.all(), many=True).data
+        return []
